263a7dab5eead54304f4c17e6f678683d019f70a
cristianmarind <cristian.marind@udea.edu.co>
2018-09-22 11:15:34 -0500

    commit initial

--- public/main.js
+++ public/main.js
@@ -1,54 +1,27 @@
 var socket = io.connect({ 'forceNew': true });
-var pStory = document.getElementById('pStory');
-var nowWord;
+var map = L.map('map').setView([6.265269699999999, -75.5668732], 13);
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+map.locate({setView: true, maxZoom: 16});
+map.on('locationfound', onLocationFound);
 
-socket.on('story', function (data) {
-	renderStory(data);
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+	socket.emit('new-carpuling', {location: e.latlng, radius: radius});
+}
+
+map.on('locationfound', onLocationFound);
+
+socket.on('position-carpuling', function (data) {
+	L.circle(data.location, data.radius).addTo(map);
 });
 
+/*
 socket.on('new-word', function (data) {
 	renderWord(data);
 });
 
-document.getElementById('enviar_historia').onclick = function () {
-	var auth = document.getElementById('author').value;
-	var mess = document.getElementById('message').value;
-	sendStory(auth, mess);	
-};
-
-function sendStory(author, text) {
-	if(author != ""){
-		if(text.toLowerCase().includes(nowWord.toLowerCase())){
-			var part = {
-				author: author,
-				text: text
-			}
-			socket.emit('sent-story', part);
-			document.getElementById('message').value = "";
-		}
-		else {
-			alert("La palabra actual no está en el texto");
-		}
-	} else {
-		alert("El nombre de usuario no puede estár vacio");	
-	}
-}
-
-function renderWord(wordGet) {
-	document.getElementById('wordText').innerHTML=wordGet;
-	nowWord = wordGet;
-}
-
-function renderStory(data) {
-	var html = data.map(function (elem, index) {
-		return (`<span class="tooltipped" data-position="top" data-delay="50" data-tooltip="Autor: ${elem.author}">${elem.text}</span>`);
-	}).join(" ");
-	document.getElementById('pStory').innerHTML = html;
-	$('.tooltipped').tooltip();
-}
-
-(function ($) {
-	$(function () {
-		$('.parallax').parallax();
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+socket.emit('sent-story', part);*/
--- server/app.js
+++ server/app.js
@@ -4,76 +4,33 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-var storyParts = [];
-
-var mongoose = require('mongoose');
-var Word=require('./Model/word');
-const dbMongo='mongodb://localhost:27017/bdStory';
-const port=8080;
-var nowWord = ""; 
+const port = 8080;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(dbMongo, function(err, res){
-    if (err) {
-        console.log(`Error al conectarse a la base de datos: ${err}`);
-    } else {
-        console.log("conecion establecida");
-    }
+
+
+/*
+app.post('', function (req, res) {
+   
 });
 
-// ingresar palabra a la bd
-app.post('/api/setWord',function(req, res){
-	let word = new Word();
-    word.word=req.param('inputWord');
-	word.save((err, storedWord) => {
-        if (err) {
-            res.status(500) // los 500 son errores del Servidor
-            res.send({ message: `Error al guardar elemento en la BD ${err}` });
-        }else {
-            res.status(200);
-            res.redirect('/'); // luego de insertar redireciona
-            res.end(); // finalizar la petición
-        }
-    });
-});
-
-app.get('/api/words', function(req, res){
-    // con el {} trae todas las palabras
-    Word.find({}, (err, words) => {
-        if (err) return res.status(500).send({ message: `Error al buscar ${err}` });
-        if (!words) return res.status(404).send({ message: `No hay palabras` });
-        res.status(200).send({ words: words });
-    });
-});
+app.get('', function (req, res) {
+});*/
 
 io.on('connection', function (socket) {
     console.log('Alguien se ha conectado con Sockets');
-    socket.emit('story', storyParts);
-    socket.emit('new-word', nowWord);
-
-    socket.on('sent-story', function (data) {
-        storyParts.push(data);
-        io.sockets.emit('story', storyParts);
-        randomWord(function (err, data) {
-            io.emit('new-word', data);
-        });
-
+    /*socket.emit('story', storyParts);
+    socket.emit('new-word', nowWord);*/
+    socket.on('new-carpuling', function (data) {
+        io.sockets.emit('position-carpuling', data);
     });
 });
 
-function randomWord(callback){
-    Word.find({}, (err, words) => {
-        var number=Math.floor(Math.random()*words.length);  
-        nowWord = words[number].word;
-        callback(0, nowWord);
-    });
-}
-
-server.listen(port, function(){
-	console.log("Corriendo por el puerto "+port)
+server.listen(port, function () {
+    console.log("Corriendo por el puerto " + port)
 });
 
