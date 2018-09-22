eafea77c3b9534354cd2ec8156dd269ec909ca42
cristianmarind <cristian.marind@udea.edu.co>
2018-09-22 11:56:39 -0500

    cambios en location

--- public/main.js
+++ public/main.js
@@ -4,15 +4,14 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-map.locate({setView: true, maxZoom: 16});
+
 map.on('locationfound', onLocationFound);
-
-
+setInterval(function(){
+	map.locate({setView: true, maxZoom: 16});
+}, 5000)
 function onLocationFound(e) {
     var radius = e.accuracy / 4;
-	setInterval(function(){
-		socket.emit('new-carpuling', {location: e.latlng, radius: radius});
-	}, 5000)
+	socket.emit('new-carpuling', {location: e.latlng, radius: radius});
 }
 
 map.on('locationfound', onLocationFound);
